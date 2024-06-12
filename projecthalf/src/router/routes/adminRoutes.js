9b0a0b8406eb3ee3833f433c55d26bc65d8d6f77
unknown <hebbarambika.k@gmail.com>
2024-06-12 16:33:04 +0530

    first commit

---
+++ projecthalf/src/router/routes/adminRoutes.js
@@ -0,0 +1,24 @@
+import { lazy } from "react";
+const Category = lazy(() => import("../../views/admin/Category"));
+
+const AdminDashboard =lazy(()=>import ( "./../../views/admin/AdminDashboard"));
+const Orders =lazy(()=>import ( "../../views/admin/Orders"));
+
+export const adminRoutes=[
+    {
+        path:'/admin/dashboard',
+        element:<AdminDashboard/>,
+        role:'admin'
+    },
+    {
+        path:'/admin/dashboard/orders',
+        element:<Orders/>,
+        role:'admin'
+    },
+    {
+        path:'/admin/dashboard/category',
+        element:<Category/>,
+        role:'admin'
+    },
+
+] 
