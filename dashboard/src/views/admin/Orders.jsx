b74ee8dd85a4257845dc310e22be1f127851a825
unknown <hebbarambika.k@gmail.com>
2024-08-12 16:55:30 +0530

    removed search bar

--- dashboard/src/views/admin/Orders.jsx
+++ dashboard/src/views/admin/Orders.jsx
@@ -35,7 +35,7 @@ const Orders = () => {
                         <option value="10">10</option>
                         <option value="20">20</option> 
                     </select>
-                    <input onChange={e => setSearchValue(e.target.value)} value={searchValue} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="text" placeholder='search' /> 
+                    {/* <input onChange={e => setSearchValue(e.target.value)} value={searchValue} className='px-4 py-2 focus:border-indigo-500 outline-none bg-[#6a5fdf] border border-slate-700 rounded-md text-[#d0d2d6]' type="text" placeholder='search' />  */}
                 </div>
 
 
